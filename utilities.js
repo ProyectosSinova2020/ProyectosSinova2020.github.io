7967be33ac3003ae74aa7d93c9857725f4a87ab3
Cuenta Sinova <proyectos@sinova.co>
2020-08-18 08:54:30 -0500

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,21 +1,49 @@
-(function(window){
-  // You can enable the strict mode commenting the following line  
+(function(window) {
+  // You can enable the strict mode commenting the following line  
   // 'use strict';
 
+
   // This function will contain all our code
-  function snvaFunctions(){
-    var _myFunctions = {};
+  function myLibrary() {
+    var _myLibraryObject = {};
 
-    // We will add functions to our library here !
+    // This variable will be inaccessible to the user, only can be visible in the scope of your library.
+    var settings = {
+      volume: 100,
+      mute: false
+    };
 
-    return _myFunctions;
+    // Change a private property
+    _myLibraryObject.setVolume = function(volume) {
+      settings.volume = volume;
+      return volume;
+    };
+
+    // Change a private property
+    _myLibraryObject.setMute = function(muteStatus) {
+      if (typeof(muteStatus) === 'boolean') {
+        settings.mute = muteStatus;
+      } else {
+        console.error("You need to disable or enable the sound !");
+      }
+
+      return settings.mute;
+    };
+
+    // Change a private property
+    _myLibraryObject.haveSound = function() {
+      return settings.mute;
+    };
+
+    return _myLibraryObject;
   }
 
   // We need that our library is globally accesible, then we save in the window
-  if(typeof(window.snvaLibrary) === 'undefined'){
-    window.snvaLibrary = snvaFunctions();
+  if (typeof(window.myWindowGlobalLibraryName) === 'undefined') {
+    window.myWindowGlobalLibraryName = myLibrary();
   }
-})(window);
+})(window); // We send the window variable withing our function
 
-console.log("Iniciando");
-console.log(snvaLibrary);
+
+// Now see the content of your library
+console.log(myWindowGlobalLibraryName);
