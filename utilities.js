fee90bfb0e9ceb1e7f61aa6bb99883f71c4efbef
Cuenta Sinova <proyectos@sinova.co>
2020-08-18 08:42:52 -0500

    Create utilities.js

---
+++ utilities.js
@@ -0,0 +1,20 @@
+(function(window){
+  // You can enable the strict mode commenting the following line  
+  // 'use strict';
+
+  // This function will contain all our code
+  function snvaFunctions(){
+    var _myFunctions = {};
+
+    // We will add functions to our library here !
+
+    return _myFunctions;
+  }
+
+  // We need that our library is globally accesible, then we save in the window
+  if(typeof(window.snvaLibrary) === 'undefined'){
+    window.snvaLibrary = snvaFunctions();
+  }
+})(window);
+
+console.log(snvaLibrary);
